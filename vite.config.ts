fe899090ae46166043727009504b7c1b0d925daf
tmair32 <tmair32@gmail.com>
2022-05-18 14:01:03 +0900

    :four_leaf_clover: Setting unplugin-vue-components
    # Reference
    [unplugin-vue-components](https://github.com/antfu/unplugin-vue-components)

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
@@ -9,6 +10,11 @@ export default defineConfig({
   plugins: [
     AutoImport({
       imports: ["vue", "@vueuse/core", "@vueuse/head"],
+      dts: true,
+    }),
+    Components({
+      extensions: ["vue", "md"],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
       dts: true,
     }),
     vue(),
