b1fe6764cd4ce46275c95f9a8ada729642721245
tmair32 <tmair32@gmail.com>
2022-05-18 14:43:13 +0900

    :four_leaf_clover: Setting Vite Plugin Pages
    # Install
    @types/nodes => fs, path
    gray-matter
    vite-plugin-pages
    
    # Reference
    https://github.com/antfu/antfu.me/blob/main/vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import WindiCSS from "vite-plugin-windicss";
 
+import { resolve } from "path";
+import { readFileSync } from "fs";
+import matter from "gray-matter";
+import Pages from "vite-plugin-pages";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -16,6 +21,29 @@ export default defineConfig({
       extensions: ["vue", "md"],
       include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
       dts: true,
+    }),
+    Pages({
+      pagesDir: [
+        {
+          dir: "src/pages",
+          baseRoute: "",
+        },
+        {
+          dir: "posts",
+          baseRoute: "posts",
+        },
+      ],
+      extensions: ["vue", "md"],
+      extendRoute(route) {
+        // Get inspired from anthony fu"s personal website
+        // https://github.com/antfu/antfu.me
+        const path = resolve(__dirname, route.component.slice(1));
+        const md = readFileSync(path, "utf-8");
+        const { data } = matter(md);
+        if (path.split(".").pop() == "md") {
+          route.meta = Object.assign(route.meta || {}, { frontmatter: data });
+        }
+      },
     }),
     vue(),
     WindiCSS(),
