cf85912eb64697c39642d842bb18f8c56fd39d4f
tmair32 <tmair32@gmail.com>
2022-05-18 16:03:38 +0900

    :star: Setting Default Style

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { ViteSSG } from "vite-ssg";
 import App from "./App.vue";
-
 import Marzipano from "marzipano";
 import routes from "virtual:generated-pages";
-import "virtual:windi.css";
 import { RouterScrollBehavior } from "vue-router";
+import "~/assets/styles/style.scss";
+import "virtual:windi.css";
 
 const scrollBehavior: RouterScrollBehavior = (to, from, savedPosition) => {
   if (savedPosition) return savedPosition;
