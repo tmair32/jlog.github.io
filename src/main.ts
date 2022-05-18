e2d6b6be49b202a784170d057bdbf60e6b2274b5
tmair32 <tmair32@gmail.com>
2022-05-18 15:47:59 +0900

    :four_leaf_clover: Setting ViteSSG & Vue Router

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
-import { createApp } from "vue";
+import { ViteSSG } from "vite-ssg";
 import App from "./App.vue";
-import "virtual:windi.css";
-import Marzipano from "marzipano";
 
-createApp(App).use(Marzipano).mount("#app");
+import Marzipano from "marzipano";
+import routes from "virtual:generated-pages";
+import "virtual:windi.css";
+import { RouterScrollBehavior } from "vue-router";
+
+const scrollBehavior: RouterScrollBehavior = (to, from, savedPosition) => {
+  if (savedPosition) return savedPosition;
+  else return { top: 0, behavior: "smooth" };
+};
+
+export const createApp = ViteSSG(App, { routes, scrollBehavior }, (ctx) => {
+  Object.values(import.meta.globEager("./module/*.ts")).map((i) =>
+    i.install?.(ctx)
+  );
+});
